397ffdea238a4ce28c77d0e9e3a63149516c424b
PiotrNowickii <s24667@pjwstk.edu.pl>
2024-10-18 12:15:34 +0200

    Made minor changes to test the scripts, changed parts of the ci.yml file, changed parts of both the scripts
    Made changes in requirements.txt
    
    Added dependencies to requirements.txt required in other repository
    
    Made changes to ci.yml file to hopefully run my python scripts
    
    More changes to ci.yml file
    
    Added another dependency to requirements.txt, Changed name of github action
    
    Made changes which will allow the scripts to execute
    
    changed part of scope
    
    Added .csv to file
    
    Made script change NaN values to None
    
    Wrote a new way to upload csv file to google sheets
    
    Commented out google sheets exporting script to see if clean_data.py works,
    
    trying to make the script standardize properly
    
    Small change to make standardization work
    
    Added lines to try save result data to google sheet
    
    Testing without standardization

--- clean_data.py
+++ clean_data.py
@@ -13,21 +13,20 @@ def random_travel_time():
         end_minute = (start_minute + int((duration % 1) * 60)) % 60
         return f"{start_hour:02}:{start_minute:02}", f"{end_hour:02}:{end_minute:02}"
 
-def main():
+if __name__ == "__main__":
     logging.basicConfig(filename='log.txt',filemode='w',level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     logger.info('Began retrieving data from google spreadsheet')
     genders = ['Mężczyzna', 'Kobieta']
     education_levels = ['Podstawowe', 'Średnie', 'Wyższe']
     travel_goals = ['Praca', 'Zakupy', 'Edukacja', 'Rozrywka', 'Inne']
-    scope = ["https://googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     credentials = Credentials.from_service_account_file('credentials.json', scopes=scope)
     client = gspread.authorize(credentials)
     sheet_id = "1-TdYTmaMyK7mPncbZE4wEiFPirj8itMS9H9s9YdKwaQ"
     worksheet = client.open_by_key(sheet_id).sheet1
-    data = worksheet.get_all_values()
-    df = pd.DataFrame(data[1:], columns=data[0])
-    logger.info('Finished retrieving data from google spreadsheet')
+    df = pd.read_csv("Lab2---Obr-bka-danych/data_student_24667.csv")
+    logger.info('Finished retrieving data from csv')
     length_before_collumn_drop = len(df)
     df = df.dropna(thresh=4)
     length_after_collumn_drop = len(df)
@@ -54,13 +53,17 @@ def main():
 
     logger.info('Began the standardization of data')
     scaler = StandardScaler()
-    df = scaler.fit_transform(df)
+    df.loc[:, ['Wiek', 'Średnie Zarobki']] = scaler.fit_transform(df.loc[:, ['Wiek', 'Średnie Zarobki']])
     logger.info('Finished the standardization of data')
 
+    worksheet.clear()
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    logger.info('Saved data to sheet')
 
     removed_percent = (removed_rows/length_before_collumn_drop) * 100
     filled_percent = (filled_rows/length_after_collumn_drop) * 100
     
+
     with open("report.txt", "w") as file:
         file.write(f"Percent of removed rows: {removed_percent:.2f}%\nPercent of changed rows: {filled_percent:.2f}%")
     logger.info(f"Removed {removed_rows} rows")
--- export_to_spreadsheet.py
+++ export_to_spreadsheet.py
@@ -4,15 +4,17 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 
-def main():
-    scope = ["https://googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+if __name__ == "__main__":
+    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     credentials = Credentials.from_service_account_file('credentials.json', scopes=scope)
     client = gspread.authorize(credentials)
     sheet_id = "1-TdYTmaMyK7mPncbZE4wEiFPirj8itMS9H9s9YdKwaQ"
     worksheet = client.open_by_key(sheet_id).sheet1
-    df = pd.read_csv("Lab2---Obr-bka-danych/data_student_24667")
-    data = [df.columns.values.tolist()] + df.values.tolist()
+    df = pd.read_csv("Lab2---Obr-bka-danych/data_student_24667.csv")
+    #df = df.where(pd.notnull(df), None)
+    #data = [df.columns.values.tolist()] + df.values.tolist()
     worksheet.clear()
-    worksheet.append_rows(data)
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    #worksheet.append_rows(data)
     
     
